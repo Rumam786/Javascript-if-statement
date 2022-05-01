1fdc391c0b91406a6322d88187d29756eb0b6a74
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-05-02 00:57:12 +0500

    Add files via upload

---
+++ family.js
@@ -0,0 +1,19 @@
+if(10 == 10)
+{
+    document.write("True");
+};
+document.write("<br>");
+if(40 === 40)
+{
+    document.write("True");
+};
+document.write("<br>");
+if(9 < 16)
+{
+    document.write("True");
+};
+document.write("<br>");
+if(56 > 37)
+{
+    document.write("True");
+};
